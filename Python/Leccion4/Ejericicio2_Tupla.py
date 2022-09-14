d79f00b51e39a0ff5c22e817fef297eda0a404fe
ezequielmariscal <ezequielramonmariscal@gmail.com>
2022-09-14 17:31:09 -0300

    agregamos tuplas mas 1 ejercicio del mismo

---
+++ Python/Leccion4/Ejericicio2_Tupla.py
@@ -0,0 +1,49 @@
+
+############ TUPLAS##########
+
+# las listas son modificables.... las tuplas aunque son similares a las lista siguen el orden de las listas
+# las tuplas se pueden modificar pero no se pueden eliminar es decir son INMUTABLES esa es la principal dif.
+
+# ***** Leccion 1 =====> Definimos una tupla
+
+cocina = ('cuchara', 'cuchillo', 'tenedor') #usamos parentesis en las tuplas.
+print(cocina)
+print(len(cocina)) # tambien podemos usar len para desaparecer la tupla
+
+# ***** Leccion 2 =====> Acceder a un elemento, para esto usamos corchetes no parentesis
+
+print(cocina[0])
+
+# ***** Leccion 3 =====> Tambien lo puedo mostrar de manera inversa
+
+print(cocina[-1])
+
+# ***** Leccion 4 =====> Acceder a un rango (quiero mostrar un grupo de elementos)
+
+print(cocina[0:1]) #aca simplemente no muestra el cero
+# como saber cuando es una tupla y cuando es un array?==?=?
+#Ejemplo
+verduras = ('papa',) # Una tupla siempre tendra las coma despues(aunq sea una sola coma) de las comillas (IMPORTANTE)
+
+# DE LO CONTRARIO SOLO SERIA UN TIPO STR CADENA..
+
+# ***** Leccion 5 =====> Recorremos los elementos de una tupla
+for cocinar in cocina:
+    print(cocinar) # al ejecutar nos muestra un salto de linea que lo hace print usa backlash /n para usar salto de lineas
+    print(cocinar, end=' ') # hace que los saltos de linea finalicen
+
+#se puede cambiar un tupla como una lista? NOOOOO!!!! salta error en el print
+#cocina[0] = 'plato'
+#print(cocina)
+
+#como se puede modificar una tupla?? ESTO ES UNA MALA PRACTICA AL MENOS QUE SEA NECESARIO
+#hacemos conversion de tupla a lista y modificamos y pasamos a tupla de vuelta
+cocinaLista = list(cocina) # de tupla a lista
+cocinaLista[0] = 'plato' # modificamos
+cocina = tuple(cocinaLista) # de lista a tupla
+print('\n', cocina)
+
+# ***** Leccion 6 =====> como elimino una tupla?
+
+#del cocina
+#print(cocina)
